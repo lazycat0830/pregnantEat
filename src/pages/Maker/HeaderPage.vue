bf16f0263005d24a593b4c79389b77bb2ddf32e3
lazycat0830 <128151359+lazycat0830@users.noreply.github.com>
2023-09-24 17:27:09 +0800

    Save HeaderPage.vue.

--- src/pages/Maker/HeaderPage.vue
+++ src/pages/Maker/HeaderPage.vue
@@ -1,1 +1,36 @@
-<template>MakerheaderPage</template>
+<template>
+    <div class=column item-center justify-center>
+<div v-for="data in AllData" ：key="data">
+            <q-card class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section>
+          
+        <q-img
+          class="col-5"
+          src="https://cdn.quasar.dev/img/parallax2.jpg"
+/>
+        </q-card-section>
+        <div>
+            <div>{{data.Title}}</div>
+            <div>
+                <q-chip v-for="food in data.foodList" :key="food" color="primary" text-color="white" icon="event">
+    {{food.value}}
+      </q-chip>
+      
+      <q-btn>查看食譜</q-btn>
+</div>
+            </div>
+        </div>
+      </q-card-section>
+    </q-card>
+    </div>
+
+</template>
+<script>
+    import {ref} form "vue";
+    
+    const AllData=ref([]);
+    AllData.value=[
+        {Title:"標題1",foodList:["食材1","食材2","食材3","食材4","食材5","食材6"]}
+        ]
+</script>
